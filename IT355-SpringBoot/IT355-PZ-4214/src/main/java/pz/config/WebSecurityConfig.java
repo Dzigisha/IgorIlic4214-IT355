a708f722eedddcf2387aaee67d275956aaaa0008
Dzigisha <igorilic55@gmail.com>
2023-07-03 13:26:09 +0200

    first commit

---
+++ IT355-SpringBoot/IT355-PZ-4214/src/main/java/pz/config/WebSecurityConfig.java
@@ -0,0 +1,41 @@
+package pz.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().cors().and().authorizeRequests().anyRequest().permitAll();
+                /*.antMatchers("/").permitAll()
+                .antMatchers(HttpMethod.POST,"/*").permitAll()
+                .antMatchers(HttpMethod.GET,"/*").permitAll()
+                .antMatchers(HttpMethod.DELETE,"/*").permitAll()
+                .antMatchers(HttpMethod.PUT,"/*").permitAll()
+                .anyRequest().authenticated();*/
+    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:4200");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+}
---
+++ IT355-SpringBoot/IT355-PZ-4214/src/test/java/com/ilic4214/it355pz/It355Pz4214ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilic4214.it355pz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class It355Pz4214ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
